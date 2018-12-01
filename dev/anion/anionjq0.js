7910ba89452717f85b9b14ff37c1028281b0851f
Peter Cross <itextx@gmail.com>
2018-12-02 00:53:36 +0300

    v2 initial

--- dev/anion/anionjq0.js
+++ dev/anion/anionjq0.js
@@ -14,23 +14,6 @@ if ($(window).width() < 600) {
   $('.main__output-area').hide();
 }
 
-// CONSTS
-// var MOL_CONSTS = {
-//   NA2O: 61.979,
-//   K2O: 94.196,
-//   AL2O3: 101.961,
-//   MGO: 40.304,
-//   FEO: 71.846,
-//   FE2O3: 159.692,
-//   CAO: 56.077,
-//   MNO: 70.938,
-//   TIO2: 79.879,
-//   SIO2: 60.084,
-//   H2O: 18.015,
-//   F: 18.994,
-//   CL: 35.453,
-// };
-
 var KEYCODES = {
   ENTER: 13,
   SPACE: 32,
@@ -183,22 +166,6 @@ var elementsArr = [
   },
 ];
 
-// var elementValues = {
-//   Na2O: 0,
-//   K2O: 0,
-//   Al2O3: 0,
-//   MgO: 0,
-//   FeO: 0,
-//   Fe2O3: 0,
-//   CaO: 0,
-//   MnO: 0,
-//   TiO2: 0,
-//   SiO2: 0,
-//   H2O: 0,
-//   F: 0,
-//   Cl: 0;
-// };
-
 // inputs collection
 var inputsColl = $('.main__input');
 
@@ -210,42 +177,6 @@ inputsColl.each((i, inputEl) => {
       $(`#${inputEl.id}`).on('input', function() {
         element.value = this.value;
       });
-      // $('#K2O-input').on('input', function() {
-      //   elementValues.K2O = this.value;
-      // });
-      // $('#Al2O3-input').on('input', function() {
-      //   elementValues.Al2O3 = this.value;
-      // });
-      // $('#MgO-input').on('input', function() {
-      //   elementValues.MgO = this.value;
-      // });
-      // $('#FeO-input').on('input', function() {
-      //   elementValues.FeO = this.value;
-      // });
-      // $('#Fe2O3-input').on('input', function() {
-      //   elementValues.Fe2O3 = this.value;
-      // });
-      // $('#CaO-input').on('input', function() {
-      //   elementValues.CaO = this.value;
-      // });
-      // $('#MnO-input').on('input', function() {
-      //   elementValues.MnO = this.value;
-      // });
-      // $('#TiO2-input').on('input', function() {
-      //   elementValues.TiO2 = this.value;
-      // });
-      // $('#SiO2-input').on('input', function() {
-      //   elementValues.SiO2 = this.value;
-      // });
-      // $('#H2O-input').on('input', function() {
-      //   elementValues.H2O = this.value;
-      // });
-      // $('#F-input').on('input', function() {
-      //   elementValues.F = this.value;
-      // });
-      // $('#Cl-input').on('input', function() {
-      //   elementValues.Cl = this.value;
-      // });
     }
   });
 });
@@ -264,10 +195,6 @@ var setToFixed = function (elem) {
 
 //calculate function
 var calculate = function () {
-  // var getMolRatio = function (elem, mol) {
-  //   var molRatio = elem / mol;
-  //   return molRatio;
-  // };
 
   elementsArr.forEach((elem) => {
     elem.molRatio = elem.value / elem.mol;
@@ -304,15 +231,9 @@ var calculate = function () {
 
   var anionRatioFactor = preAnionRatioFactor / mineralSelectValue;
 
-  // console.log({anionRatioFactor});
-
   elementsArr.forEach((elem) => {
     elem.apfu = elem.cationRatio / anionRatioFactor;
   });
-
-  console.log(elementsArr);
-  // console.log(anionRatioFactor);
-
 
   var outputsColl = $('.main__output');
   outputsColl.each((i, outputEl) => {
@@ -324,92 +245,6 @@ var calculate = function () {
     });
   });
 
-  console.log(outputsColl);
-
-  // var Na2O_MolRatio = getMolRatio(Na2O, NA2O_MOL);
-  // var K2O_MolRatio = getMolRatio(K2O, K2O_MOL);
-  // var Al2O3_MolRatio = getMolRatio(Al2O3, AL2O3_MOL);
-  // var MgO_MolRatio = getMolRatio(MgO, MGO_MOL);
-  // var FeO_MolRatio = getMolRatio(FeO, FEO_MOL);
-  // var Fe2O3_MolRatio = getMolRatio(Fe2O3, FE2O3_MOL);
-  // var CaO_MolRatio = getMolRatio(CaO, CAO_MOL);
-  // var MnO_MolRatio = getMolRatio(MnO, MNO_MOL);
-  // var TiO2_MolRatio = getMolRatio(TiO2, TIO2_MOL);
-  // var SiO2_MolRatio = getMolRatio(SiO2, SIO2_MOL);
-  // var H2O_MolRatio = getMolRatio(H2O, H2O_MOL);
-  // var F_MolRatio = getMolRatio(F, F_MOL);
-  // var Cl_MolRatio = getMolRatio(Cl, CL_MOL);
-  //
-  // // anion ratios
-  // var Na2O_anionRatio = Na2O_MolRatio;
-  // var K2O_anionRatio = K2O_MolRatio;
-  // var Al2O3_anionRatio = Al2O3_MolRatio * 3;
-  // var MgO_anionRatio = MgO_MolRatio;
-  // var FeO_anionRatio = FeO_MolRatio;
-  // var Fe2O3_anionRatio = Fe2O3_MolRatio * 3;
-  // var CaO_anionRatio = CaO_MolRatio;
-  // var MnO_anionRatio = MnO_MolRatio;
-  // var TiO2_anionRatio = TiO2_MolRatio * 2;
-  // var SiO2_anionRatio = SiO2_MolRatio * 2;
-  // var H2O_anionRatio = H2O_MolRatio;
-  // var F_anionRatio = F_MolRatio;
-  // var Cl_anionRatio = Cl_MolRatio;
-  //
-  // // cation ratios
-  // var Na2O_cationRatio = Na2O_MolRatio * 2;
-  // var K2O_cationRatio = K2O_MolRatio * 2;
-  // var Al2O3_cationRatio = Al2O3_MolRatio * 2;
-  // var MgO_cationRatio = MgO_MolRatio;
-  // var FeO_cationRatio = FeO_MolRatio;
-  // var Fe2O3_cationRatio = Fe2O3_MolRatio * 2;
-  // var CaO_cationRatio = CaO_MolRatio;
-  // var MnO_cationRatio = MnO_MolRatio;
-  // var TiO2_cationRatio = TiO2_MolRatio;
-  // var SiO2_cationRatio = SiO2_MolRatio;
-  // var H2O_cationRatio = H2O_MolRatio * 2;
-  // var F_cationRatio = F_MolRatio;
-  // var Cl_cationRatio = Cl_MolRatio;
-  //
-  // // prosto anion rating
-  // var getAnionRatioFactor = function (divisor) {
-  //   return (Na2O_anionRatio + K2O_anionRatio + Al2O3_anionRatio + MgO_anionRatio + FeO_anionRatio + Fe2O3_anionRatio + CaO_anionRatio + MnO_anionRatio + TiO2_anionRatio + SiO2_anionRatio + H2O_anionRatio + F_anionRatio + Cl_anionRatio - ((F_anionRatio + Cl_anionRatio)/2)) / divisor;
-  // }
-  //
-  // var getApfu = function (mineral) {
-  //   return mineral / getAnionRatioFactor(mineralSelectValue);
-  // }
-  //
-  // // apfu
-  // var Na2O_apfu = getApfu(Na2O_cationRatio);
-  // var K2O_apfu = getApfu(K2O_cationRatio);
-  // var Al2O3_apfu = getApfu(Al2O3_cationRatio);
-  // var MgO_apfu = getApfu(MgO_cationRatio);
-  // var FeO_apfu = getApfu(FeO_cationRatio);
-  // var Fe2O3_apfu = getApfu(Fe2O3_cationRatio);
-  // var CaO_apfu = getApfu(CaO_cationRatio);
-  // var MnO_apfu = getApfu(MnO_cationRatio);
-  // var TiO2_apfu = getApfu(TiO2_cationRatio);
-  // var SiO2_apfu = getApfu(SiO2_cationRatio);
-  // var H2O_apfu = getApfu(H2O_cationRatio);
-  // var F_apfu = getApfu(F_cationRatio);
-  // var Cl_apfu = getApfu(Cl_cationRatio);
-  //
-  //
-  // // setting outputs values
-  // $('#Na2O-output').text(setToFixed(Na2O_apfu));
-  // $('#K2O-output').text(setToFixed(K2O_apfu));
-  // $('#Al2O3-output').text(setToFixed(Al2O3_apfu));
-  // $('#MgO-output').text(setToFixed(MgO_apfu));
-  // $('#FeO-output').text(setToFixed(FeO_apfu));
-  // $('#Fe2O3-output').text(setToFixed(Fe2O3_apfu));
-  // $('#CaO-output').text(setToFixed(CaO_apfu));
-  // $('#MnO-output').text(setToFixed(MnO_apfu));
-  // $('#TiO2-output').text(setToFixed(TiO2_apfu));
-  // $('#SiO2-output').text(setToFixed(SiO2_apfu));
-  // $('#H2O-output').text(setToFixed(H2O_apfu));
-  // $('#F-output').text(setToFixed(F_apfu));
-  // $('#Cl-output').text(setToFixed(Cl_apfu));
-  //
   //media settings
   if ($(window).width() < 600) {
     $('.main__output-area').slideDown('fast', function () {
@@ -476,33 +311,6 @@ var clear = () => {
     $('.main__output-area').slideUp('fast');
   }
 };
-
-// var clear = function () {
-//   Na2O = 0;
-//   K2O = 0;
-//   Al2O3 = 0;
-//   MgO = 0;
-//   FeO = 0;
-//   Fe2O3 = 0;
-//   CaO = 0;
-//   MnO = 0;
-//   TiO2 = 0;
-//   SiO2 = 0;
-//   H2O = 0;
-//   F = 0;
-//   Cl = 0;
-//   $('.main__input').val('');
-//   $('.main__output').text('');
-//   inputsArr.length = 0;
-//
-//   $('.button--calculate-anion').prop('disabled', true);
-//
-//   //media settings
-//   if ($(window).width() < 600) {
-//     $('.main__output-area').slideUp('fast');
-//   }
-//
-// };
 
 // setting inputs keypress event
 $(document).keydown(function(e) {
